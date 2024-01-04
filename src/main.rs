e3e240c08e8a08c39f6a8c4271ae67d34f14cd78
hezz <hezhaozhaog@gmail.com>
2024-01-04 23:04:36 +0800

    test kitty http proxy

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ use anyhow::Result;
 use env_logger;
 use kitty_proxy::{HttpProxy, MatchProxy};
 use log::info;
+use url::Url;
 use std::str::FromStr;
 use std::{path::PathBuf, sync::Arc};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init(); // 初始化日志记录器
+    println!("{:?}", Url::parse("http://www.baidu.com:443")?.host().map(|x| x.to_string()));
+    println!("{:?}", Url::parse("http://www.baidu.com")?.host());
+    println!("{:?}", Url::parse("https://www.baidu.com")?.host());
+    println!("{:?}", Url::parse("http://127.0.0.1:443")?.host());
+    println!("{:?}", Url::parse("http://127.0.0.1/aaa")?.host());
+
+    println!("{:?}", Url::parse("http://www.baidu.com:443")?.port());
+    println!("{:?}", Url::parse("http://www.baidu.com")?.port());
+    println!("{:?}", Url::parse("https://www.baidu.com")?.port());
+    println!("{:?}", Url::parse("http://127.0.0.1:443")?.port());
+    println!("{:?}", Url::parse("http://127.0.0.1/aaa")?.port());
 
     info!("This is an info log message.");
     let mut proxy = HttpProxy::new("127.0.0.1", 10088, None, "127.0.0.1", 10809).await?;
